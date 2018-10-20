55cdd245cc3a5207bcfe26a0b869e81f8e962686
yuhongzhang <zhangyuhong812@163.com>
2018-10-20 23:07:17 +0800

    vector 2010 调试

--- CppTest/CppTest/CppTest.cpp
+++ CppTest/CppTest/CppTest.cpp
@@ -2,11 +2,50 @@
 //
 
 #include "stdafx.h"
+#include <Windows.h>
 
 
-int _tmain(int argc, _TCHAR* argv[])
+#include <string>
+
+#include <iostream>
+using namespace std;
+ 
+/*
+*多行注释，每一行都以一个*开头；
+*
+*/
+#include <vector>
+
+
+int main()
 {
+	printf("the world is peace!\n");
+	int v1 = 1;
+	int v2 = 2;
+	
+	cout << "the sum of" << v1
+				<< "and" << v2
+	 			<< "is"  << v1 + v2 << endl;
+
+	/*vector 练习*/
+
+	vector<int>  v(5);
+	//vector<string>  s(5,11);
+	int sumCnt = 0;
+
+	v[0] = 100;
+	v[1] = 100;
+	v[2] = 100;
+	v[3] = 100;
+	v[4] = 100;
+	/// v[5]= 100;
+	//for(int i  = 0;i < 5;i++)
+	sumCnt = v.size();
+	 
+	cout << sumCnt << endl;
+	system("pause");
 	return 0;
+
 }
 
 //add new info.  ---zhangyuhong_20181001
